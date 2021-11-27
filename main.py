8951842a29242a735ab33514d7b58e4f954a2edc
Sylivan Kenobi <sypsilon@hotmail.com>
2021-11-27 20:25:52 +0100

    add name, description and evolution to canvas

--- main.py
+++ main.py
@@ -4,35 +4,93 @@ from PIL import Image
 from PIL import ImageFilter
 import urllib.request
 import climage
-import pokebase
-
-charmander = pokebase.pokemon('4')
+import climage
 
 def create_canvas():
     canvas = []
     for height in range(20):
         line = []
         for width in range(80):
-            if width >= 40:
-                line.append(210)
-            else:
-                line.append(0)
+            line.append([0, " "])
         canvas.append(line)
     return canvas
 
 def print_color(fg=0, bg=255, text=" "):
     return f'\033[48;5;{bg}m\033[38;5;{fg}m{text}\033[0;0m'
 
+def add_to_canvas(canvas, placement, content):
+    place = {
+        'description': {'columns': 25, 'lines': 16, 'start_line': 8, 'start_column': 8},
+        'name': {'columns': 25, 'lines': 1, 'start_line': 2, 'start_column': 8},
+        'evo': {'columns': 80, 'lines': 4, 'start_line': 16, 'start_column': 4}
+    }
+    placement = place[placement]
+    content = list(content)
+    content_index = 0
+    done = False
+    # TODO: find better way to loop and break
+    for line in range(placement['start_line'], placement['start_line']+placement['lines']):
+        for column in range(placement['start_column'],placement['start_column']+placement['columns']):
+            if content[content_index] == '\n':
+                content_index += 1
+                break
+            canvas[line][column][1] = content[content_index]
+            content_index += 1
+            if content_index >= len(content):
+                done = True
+                break
+        if done:
+            break
+    return canvas
+
+def get_evolutions():
+    # TODO: get evolution id dynamically
+    response = requests.get("https://pokeapi.co/api/v2/evolution-chain/2").json()
+    starter = response.get('chain').get('species').get('name')
+    # TODO: is looping necessary? fix error handling for non level evolutions
+    for i in response.get('chain').get('evolves_to'):
+        evo_1 = { 
+            'name': i.get('species').get('name'), 
+            'level': i.get('evolution_details')[0].get('min_level'), 
+            'trigger': i.get('evolution_details')[0].get('trigger').get('name'),
+            'item': (i.get('evolution_details')[0].get('held_item') or {'name': 'nothing'}).get('name')
+        }
+        for j in i.get('evolves_to'):
+            evo_2 = { 
+                'name': j.get('species').get('name'), 
+                'level': j.get('evolution_details')[0].get('min_level'), 
+                'trigger': j.get('evolution_details')[0].get('trigger').get('name'),
+                'item': (j.get('evolution_details')[0].get('held_item') or {'name': 'nothing'}).get('name')
+            }
+    return (starter, evo_1, evo_2)
+
+def create_evolutions_string(evolutions):
+    # TODO: more styling, not all evolutins are level based, not all pokemon have 3 evolutions, create spaces dynamically 
+    return f"""
+            {evolutions[0]}     >>      {evolutions[1].get('name')}     >>      {evolutions[2].get('name')}
+                        level {evolutions[1].get('level')}                level {evolutions[2].get('level')}
+            """
+
 canvas = create_canvas()
+
+
+response = requests.get("https://pokeapi.co/api/v2/pokemon-species/4")
+json = response.json()
+# TODO: get index of language dynamically
+poke_desc = json.get('flavor_text_entries')[0].get('flavor_text').replace('\u000c', ' ')
+evolutions = get_evolutions()
+evolutions = create_evolutions_string(evolutions)
+canvas = add_to_canvas(canvas, 'description', poke_desc)
+canvas = add_to_canvas(canvas, 'name', json.get('name').title())
+canvas = add_to_canvas(canvas, 'evo', evolutions)
+
 text = ""
 for line in canvas:
     for field in line:
-        text += print_color(255, field, "a")
+        text += print_color(255, field[0], field[1])
     text += "\n"
 print(text)
 
-response = requests.get("https://pokeapi.co/api/v2/pokemon/4")
-print(response.json().keys())
 # image_url_og = pokemon.get("sprites").get("front_default")
 # # urllib.request.urlretrieve(image_url_og, 'img.png')
 # # img = Image.open('img.png')
